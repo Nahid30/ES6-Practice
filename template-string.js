8a077f1336f2bf5a87dcb062290bbab59616ef53
Nahid30 <nahidshuvo30@gmail.com>
2022-02-21 04:08:50 +0600

    template-string backtik first use and knew

---
+++ template-string.js
@@ -0,0 +1,33 @@
+
+/* onst priya = 'asif akbar';
+const meye = 'meye tmi ki dhukkho chino ';
+const kobita = `kobita tmi shopno charini`; */
+
+const multiline = 'this is my first line \n' +
+'this is my second line \n' +
+'third line text here\n' + 
+'fourth line text here' ;
+
+// console.log(multiline);
+
+const multiLineNew = `this is multiline 
+this is second line mama
+this is thirrd line mama 
+this is fourth line bro
+
+`
+console.log(multiLineNew);
+
+const friends = ['abul', 'babul', 'kabul','sabul'];
+const count = 5;
+const old = '<h3 class= "friend-name"> Friend-5 </h3> ' 
+const old2 = '<h3 class"friend-name">Friend-' + count + '</h3>';
+
+const new1 = `<h3 class"friend-name">Friend-${count}' + count + '</h3>`;
+
+const first ='mamun';
+const last = 'chowdhury ';
+const fullOld = first+ " " +last;
+
+const fullNew =`this person name is ${first} ${last}`;
+console.log(fullNew);
