7545c8c34ced8ece9da342c9ae6c93f1747a4c03
Nahid30 <nahidshuvo30@gmail.com>
2022-02-22 03:56:27 +0600

    practice api

---
+++ arrow-function.js
@@ -0,0 +1,15 @@
+// function declare
+
+function add (num1, num2){
+    return num1 + num2;
+
+}
+
+// function decalere another away
+document.getElementById('my-btn').onclick = function handleEven (){
+
+}
+
+const sum1 = add(15,17);
+console.log(sum1);
+
